6c200cc8e664bdfa620596e1c68d3e0c220f0dff
Vchiboleca <virgilio.chiboleca@gmail.com>
2023-05-08 04:12:07 +0200

    Depois de adicionar o jfxtabpane e deu erro

---
+++ src/co/mz/hola/telas/Main.java
@@ -0,0 +1,48 @@
+package co.mz.hola.telas;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Virgilio Chiboleca
+ */
+public class Main extends Application {
+    double x,y = 0;
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        
+        stage.initStyle(StageStyle.UNDECORATED);
+        
+        root.setOnMousePressed(event ->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+            
+        });
+        
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+            
+        });
+        
+        stage.setScene(new Scene(root));
+        stage.setTitle("Hola v1.0");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/co/mz/hola/telas/PrincipalController.java
@@ -0,0 +1,133 @@
+package co.mz.hola.telas;
+
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * FXML Controller class
+ *
+ * @author Virgilio Chiboleca
+ */
+public class PrincipalController implements Initializable {
+
+    @FXML
+    private ImageView sair, menu;
+    
+    @FXML
+    private AnchorPane pane1, pane2;
+
+    @FXML
+    private StackPane contentor;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        sair.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+        
+        pane1.setVisible(false);
+        
+        
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), pane1);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+        
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), pane2);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+        
+        menu.setOnMouseClicked(event ->{
+        
+            pane1.setVisible(true);
+            
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5), pane1);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(0.15);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), pane2);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();
+        });
+        
+        pane1.setOnMouseClicked(event -> {
+            
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.5), pane1);
+            fadeTransition1.setFromValue(0.15);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+            
+            fadeTransition1.setOnFinished(event1 -> {
+                pane1.setVisible(false);
+            });
+            
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.5), pane2);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();
+            
+        });
+        
+        try {
+            Parent painel = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        } catch (IOException ex) {
+            Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+
+    
+        public void dashboard(javafx.event.ActionEvent actionEvent) throws IOException{
+            Parent painel = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        }
+        
+        public void facturas(javafx.event.ActionEvent actionEvent) throws IOException{
+            Parent painel = FXMLLoader.load(getClass().getResource("Facturas.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        }
+        
+        public void clientes(javafx.event.ActionEvent actionEvent) throws IOException{
+            Parent painel = FXMLLoader.load(getClass().getResource("Clientes.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        }
+        
+        public void fornecedores(javafx.event.ActionEvent actionEvent) throws IOException{
+            Parent painel = FXMLLoader.load(getClass().getResource("Fornecedores.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        } 
+        
+        public void cadastro(javafx.event.ActionEvent actionEvent) throws IOException{
+            Parent painel = FXMLLoader.load(getClass().getResource("Cadastro.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        }
+        
+        public void relatorio(javafx.event.ActionEvent actionEvent) throws IOException{
+            Parent painel = FXMLLoader.load(getClass().getResource("Relatorio.fxml"));
+            contentor.getChildren().removeAll();
+            contentor.getChildren().setAll(painel);
+        } 
+       
+}
