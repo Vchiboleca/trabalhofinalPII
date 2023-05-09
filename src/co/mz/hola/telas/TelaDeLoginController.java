09df2981812678542021cabf10559a553032c1e4
Vchiboleca <virgilio.chiboleca@gmail.com>
2023-05-10 01:47:21 +0200

    Depois de login

--- src/co/mz/hola/telas/TelaDeLoginController.java
+++ src/co/mz/hola/telas/TelaDeLoginController.java
@@ -19,6 +19,10 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import java.sql.*;
 import co.mz.hola.dal.ModuloConexao;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.Label;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -26,72 +30,83 @@ import co.mz.hola.dal.ModuloConexao;
  * @author Virgilio Chiboleca
  */
 public class TelaDeLoginController implements Initializable {
-    @FXML
-    private ImageView sair;
-    
-    @FXML
-    private ImageView menu;
-    @FXML
-    private ImageView btnCadastro2;
-    @FXML
-    private AnchorPane pane1;
-    @FXML
-    private AnchorPane pane2;
-    @FXML
-    private StackPane contentor;
-    @FXML
-    private ImageView btnCadastro1;
-    @FXML
-    private JFXButton btnDashboard;
-    @FXML
-    private JFXButton sairLogin;
-    
+
     Connection conexao = null;
     PreparedStatement pst = null;
     ResultSet rs = null;
-    
-    
-    
+
+    @FXML
+    private Label lblStatus;
+    @FXML
+    private JFXTextField tfNomeUsuario;
+    @FXML
+    private JFXPasswordField tfSenhaUsuario;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-           conexao = ModuloConexao.conector();
-           //Teste conexao
-           //System.out.println(conexao);
-    } 
-    
-    
+        conexao = ModuloConexao.conector();
+        //Teste conexao
+        //System.out.println(conexao);
+        if (conexao != null) {
+            lblStatus.setText("Conectado");
+
+        } else {
+            lblStatus.setText("Nao conectado");
+        }
+    }
+
     @FXML
-    private void sairLogin(ActionEvent event){
+    private void sairLogin(ActionEvent event) {
         System.exit(0);
     }
-    
+
     @FXML
-    private void entrar(MouseEvent event1) throws IOException{
-        
-        Stage stage = new Stage();
-        
-        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
-        
-        stage.initStyle(StageStyle.UNDECORATED);
-        
-        final double[] x = {0};
-        final double[] y = {0};
-         
-    root.setOnMousePressed(event ->{
-        x[0] = event.getSceneX();
-        y[0] = event.getSceneY();
-    });
-        
-    root.setOnMouseDragged(event -> {
-        stage.setX(event.getScreenX() - x[0]);
-        stage.setY(event.getScreenY() - y[0]);   
-    });
-        
-        stage.setScene(new Scene(root));
-        stage.setTitle("Hola v1.0");
-        stage.show();
-       ((Node)event1.getSource()).getScene().getWindow().hide();
-        
+    private void entrar(MouseEvent event1) throws IOException {
+
+        String sql = "SELECT * FROM tbusuarios WHERE usuario = ? AND senha = ? ";
+
+        try {
+            //Preparacao da consulta a base de dados
+            pst = conexao.prepareStatement(sql);
+            pst.setString(1, tfNomeUsuario.getText());
+            pst.setString(2, tfSenhaUsuario.getText());
+
+            //Execusao da query
+            rs = pst.executeQuery();
+
+            //Verifica se existe o usuario e a senha correspondente
+            if (rs.next()) {
+                Stage stage = new Stage();
+
+                Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+
+                stage.initStyle(StageStyle.UNDECORATED);
+
+                final double[] x = {0};
+                final double[] y = {0};
+
+                root.setOnMousePressed(event -> {
+                    x[0] = event.getSceneX();
+                    y[0] = event.getSceneY();
+                });
+
+                root.setOnMouseDragged(event -> {
+                    stage.setX(event.getScreenX() - x[0]);
+                    stage.setY(event.getScreenY() - y[0]);
+                });
+
+                stage.setScene(new Scene(root));
+                stage.setTitle("Hola v1.0");
+                stage.show();
+                ((Node) event1.getSource()).getScene().getWindow().hide();
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuario e/ou senha invalido");
+            }
+        } catch (Exception e) {
+            
+            JOptionPane.showMessageDialog(null, e);
+        }
+
     }
-    
+
 }
